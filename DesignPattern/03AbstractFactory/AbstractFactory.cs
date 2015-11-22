0ee2600a1bc5d7f1e19d108fde15463d99f64ce3
supperwu <supperwu4781@hotmail.com>
2015-11-22 12:21:24 +0800

    add abstract pattern

---
+++ DesignPattern/03AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,45 @@
+﻿
+namespace GangOfFour.AbstractFactory
+{
+    /// <summary>
+    /// The 'AbstractFactory' abstract class
+    /// </summary>
+    public abstract class AbstractFactory
+    {
+        protected AbstractFactory() { }
+
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+
+
+    /// <summary>
+    /// The 'ConcreteFactory1' class
+    /// </summary>
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteFactory2' class
+    /// </summary>
+    public class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+}
---
+++ DesignPattern/03AbstractFactory/AbstractProduct.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+namespace GangOfFour.AbstractFactory
+{
+    /// <summary>
+    /// The 'AbstractProductA' abstract class
+    /// </summary>
+    public abstract class AbstractProductA
+    {
+        protected AbstractProductA() { }
+    }
+
+    /// <summary>
+    /// The 'AbstractProductB' abstract class
+    /// </summary>
+    public abstract class AbstractProductB
+    {
+        protected AbstractProductB() { }
+
+        public abstract void Interact(AbstractProductA a);
+    }
+
+    /// <summary>
+    /// The 'ProductA1' class
+    /// </summary>
+    public class ProductA1 : AbstractProductA
+    {
+        public override string ToString()
+        {
+            return "ProductA1";
+        }
+    }
+
+
+    /// <summary>
+    /// The 'ProductB1' class
+    /// </summary>
+    public class ProductB1 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(string.Format("{0} interacts with {1}", this, a));
+        }
+
+        public override string ToString()
+        {
+            return "ProductB1";
+        }
+    }
+
+
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+    public class ProductA2 : AbstractProductA
+    {
+        public override string ToString()
+        {
+            return "ProductA2";
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+    public class ProductB2 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(string.Format("{0} interacts with {1}", this, a));
+        }
+
+        public override string ToString()
+        {
+            return "ProductB2";
+        }
+    }
+}
