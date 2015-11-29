050acbb42bca21ab5da09804cda760fa93d74c12
supperwu <supperwu4781@hotmail.com>
2015-11-29 20:13:48 +0800

    add adapter pattern in real world

---
+++ DesignPattern/06AdapterRealWorld/Compound.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GangOfFour.Adapter.RealWorld
+{
+    public class Compound
+    {
+        /// <summary>
+        /// 化学物
+        /// </summary>
+        protected string _chemical;
+
+        /// <summary>
+        /// 沸点
+        /// </summary>
+        protected float _boilingPoint;
+        
+        /// <summary>
+        /// 熔点
+        /// </summary>
+        protected float _meltingPoint;
+
+        /// <summary>
+        /// 分子量
+        /// </summary>
+        protected double _molecularWeight;
+
+        /// <summary>
+        /// 分子式
+        /// </summary>
+        protected string _molecularFormula;
+
+        // Constructor
+        public Compound(string chemical)
+        {
+            this._chemical = chemical;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("\nCompound: {0} ------ ", _chemical);
+        }
+    }
+}
---
+++ DesignPattern/06AdapterRealWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GangOfFour.Adapter.RealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Non-adapted chemical compound
+            var unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+            var water = new RichCompound("Water");
+            water.Display();
+
+            var benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            var ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
